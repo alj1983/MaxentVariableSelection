c55dae346231fc0d85753e067c851045cdc4e1e7
Alexander Jueterbock <Alexander-Jueterbock@web.de>
2015-03-30 16:07:34 +0200

    Created an R package structure with all the required files

---
+++ R/Maxentrun.R
@@ -0,0 +1,11 @@
+Maxentrun <-
+function(maxent,outdir,gridfolder,occurrencesites,backgroundsites,additionalargs){
+
+      system(paste("mkdir", outdir,sep=" "))
+      system(paste("nohup java -jar", maxent,
+      "-o", outdir,
+      "-j", gridfolder,
+      "-s", occurrencesites,
+      "-e", backgroundsites,
+      additionalargs,"&",sep=" "))
+      }
---
+++ R/Subsetselection.R
@@ -0,0 +1,13 @@
+Subsetselection <-
+function(outdir,lowerexclusionthreshold){
+    Resulttable <- read.csv(paste(outdir,"/maxentResults.csv",sep=""),sep=",",header=TRUE)
+    # Select those columns where the column names contain the word "contribution"
+    variable.contributions <- Resulttable[,(colnames(Resulttable) %in% grep("contribution",colnames(Resulttable),value=T))]
+    # remove the '.contribution' tag from the result names
+    colnames(variable.contributions) <- gsub(".contribution","",colnames(variable.contributions))
+    # order the variable contributions so that the most influential variable is the first one
+    variable.contributions <- variable.contributions[,order(variable.contributions,decreasing=TRUE)]
+    # Discard variables with a percentage contribution below the 'lowerexclusionthreshold'
+    variable.contributions.selected <- variable.contributions[,variable.contributions>lowerexclusionthreshold]
+    return(variable.contributions.selected)
+    }
