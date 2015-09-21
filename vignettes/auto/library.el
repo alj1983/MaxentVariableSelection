a61c4069d815355cf33c89f1f1a48e2b0afc26c1
Alexander Jueterbock <Alexander-Jueterbock@web.de>
2015-09-21 22:18:27 +0200

    Added .png files and .bib file that accompany the vignette

---
+++ vignettes/auto/library.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "library"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "Warren2011"
+     "Warren2014"
+     "Hijmans2005"
+     "Tyberghein2012"
+     "Jueterbock2015"
+     "Hijmans2015"
+     "Akaike1974"
+     "Fielding1997"
+     "Warren2010"
+     "Jimenez2012")))
+
